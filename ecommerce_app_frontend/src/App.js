a91a69d52d50240ad05b982ec8a3c49e8f54f5b4
Michael Truong <michaeltruong@Michaels-MacBook-Pro.local>
2020-12-27 15:23:29 -0500

    added Product and Rating react components

--- ecommerce_app_frontend/src/App.js
+++ ecommerce_app_frontend/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import Product from "./Components/Product";
 import data from "./data";
 import './App.css';
 
-function App() {
+function App() { 
   return (
     <div className="grid-container">
       <header>
@@ -19,31 +20,14 @@ function App() {
           {
             data.products.map((product) => {
               return (
-                <div key={product._id} className="card">
-                  <a href={`/product/${product._id}`}>
-                    <img className="medium" src={product.image} alt={product.name} />
-                  </a>
-                  <div className="card-body">
-                    <a href={`/product/${product._id}`}>
-                      <h2>{product.name}</h2>
-                    </a>
-                    <div className="rating">
-                      <span><i className="fa fa-star"></i></span>
-                      <span><i className="fa fa-star"></i></span>
-                      <span><i className="fa fa-star"></i></span>
-                      <span><i className="fa fa-star-o"></i></span>
-                      <span><i className="fa fa-star-o"></i></span>
-                    </div>
-                    <div className="price">${product.price}</div>
-                  </div>
-                </div>
+                <Product key={product._id} product={product}/> 
               )
             })
           }
         </div>
       </main>
       <footer className="row center">
-        Mike's E-Commerce Portfolio Storepage
+        E-Commerce Store App 
       </footer>
     </div>
   );
